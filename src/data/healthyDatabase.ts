1aeba77df9b6a88677460cdce203d930b6415a13
kanunal99-jpg <kanunal99@gmail.com>
2026-09-06 23:32:11 +0300

    feat(food): merge Turkish home meals into healthy database

--- src/data/healthyDatabase.ts
+++ src/data/healthyDatabase.ts
@@ -2,14 +2,15 @@
 import { FoodItem, Recipe } from '../types';
 import catalogData from './healthy_catalog.json';
 import { normalizeFoods, normalizeRecipes } from './healthyCatalogNormalizer';
+import { TURKISH_HOME_FOODS, TURKISH_HOME_RECIPES } from './turkishHomeFoods';
 
-export const HEALTHY_FOODS_CATALOG: FoodItem[] = normalizeFoods(catalogData.foods);
-export const HEALTHY_RECIPES_CATALOG: Recipe[] = normalizeRecipes(catalogData.recipes);
+export const HEALTHY_FOODS_CATALOG: FoodItem[] = normalizeFoods([...catalogData.foods, ...TURKISH_HOME_FOODS]);
+export const HEALTHY_RECIPES_CATALOG: Recipe[] = normalizeRecipes([...catalogData.recipes, ...TURKISH_HOME_RECIPES]);
 
 export const HEALTHY_DATABASE_STATS = {
   version: catalogData.version,
   updatedAt: catalogData.updatedAt,
-  foodCount: catalogData.foodCount,
-  recipeCount: catalogData.recipeCount,
-  totalEntries: catalogData.totalEntries,
+  foodCount: HEALTHY_FOODS_CATALOG.length,
+  recipeCount: HEALTHY_RECIPES_CATALOG.length,
+  totalEntries: HEALTHY_FOODS_CATALOG.length + HEALTHY_RECIPES_CATALOG.length,
 };
