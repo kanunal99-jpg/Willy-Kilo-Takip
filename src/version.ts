b1e0359b6c31396ac5273ba632c1fbd2a9c2fb13
kanunal99-jpg <kanunal99@gmail.com>
2026-09-07 16:36:31 +0300

    chore(release): align app metadata to OTA bootstrap 1.0.23

--- src/version.ts
+++ src/version.ts
@@ -9,16 +9,17 @@ export interface AppVersionInfo {
 }
 
 export const APP_VERSION: AppVersionInfo = {
-  versionName: '1.0.22',
-  versionCode: 23,
+  versionName: '1.0.23',
+  versionCode: 24,
   releaseDate: '2026-09-07',
   minSupportedVersion: '1.0.3',
-  apkUrl: 'https://github.com/kanunal99-jpg/Willy-Kilo-Takip/releases/download/v1.0.22/WillyKiloTakip-v1.0.22.apk',
+  apkUrl: 'https://github.com/kanunal99-jpg/Willy-Kilo-Takip/releases/download/v1.0.23/WillyKiloTakip-v1.0.23.apk',
   githubReleaseUrl: 'https://github.com/kanunal99-jpg/Willy-Kilo-Takip/releases/latest',
   releaseNotes: [
     'OTA güncelleme akışı Android cihazlarda sistem tarayıcısı/indirme yöneticisine yönlendirildi',
     'Uygulama içi sürüm bilgisi Android release sürümüyle hizalandı',
     'APK indirme bağlantısı sürüme özel imzalı release varlığına sabitlendi',
     'Önceki sürüm metadata uyuşmazlığı giderildi',
+    'OTA düzeltmesini taşıyan Android bootstrap release: 1.0.23 / versionCode 24',
   ],
 };
